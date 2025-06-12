1d65edb3a66dce44ae5d5c8def9cbdf25b3d1989
growingpenguin <sweetyleah0@gmail.com>
2025-06-13 00:41:07 +0900

    Update Game.js
    Less width between cookies

--- js/Game.js
+++ js/Game.js
@@ -31,7 +31,7 @@ window.addEventListener("DOMContentLoaded", () => {
   function createCookies() {
     const startX = canvas.width - 400;
     const startY = 122;
-    const gapY = 200;
+    const gapY = 160;
 
     COOKIE_NAMES.forEach((name, i) => {
       const img = new Image();
